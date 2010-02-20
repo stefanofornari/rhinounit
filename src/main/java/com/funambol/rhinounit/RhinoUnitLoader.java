defd8b478185ba5f321385e0fe2ccbb16b902116
Stefano <ste@localhost.localdomain>
2010-02-20 11:31:35 +0100

    added javadoc

--- src/main/java/com/funambol/rhinounit/RhinoUnitLoader.java
+++ src/main/java/com/funambol/rhinounit/RhinoUnitLoader.java
@@ -1,6 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright Stefano Fornari 2009
+ *
+ * This file is part of RhinoUnit.
+ *
+ * RhinoUnit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.funambol.rhinounit;
 
@@ -11,8 +25,10 @@ import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
 
 /**
+ * This is a utility class that can be used to load RhinoUnit scripts from
+ * the classpath.
  *
- * @author ste
+ * @author Stefano Fornari
  */
 public class RhinoUnitLoader {
 
@@ -29,7 +45,16 @@ public class RhinoUnitLoader {
     public RhinoUnitLoader() {
     }
 
-    public String loadBootstrapScripts() throws IOException {
+    /**
+     * Returns a script containing the entire RhinoUnit framework.
+     * Single RhinoUnit scripts are loaded from the classpath so that the
+     * framework can be easily used simply adding the jar in the classpath
+     *
+     * @return the RhinoUnit framework in a single script
+     *
+     * @throws IOException if the framework could not be loaded from classpath
+     */
+    public String getRhinoUnit() throws IOException {
         StringBuffer ret = new StringBuffer();
 
         InputStream is = null;
